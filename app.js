984c0d15cc01b5de2bcdc9536a54ed580e67c1d2
Vermi1iongodv <vermi1iongodv@gmail.com>
2024-11-17 11:34:13 +0200

    some changes

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
 "use strict";
 
-alert("Hello, world!");
+const numbersOfFilms = +prompt("how many films you watched?", "");
 
+
+
+
+const personalMovieDB = {
+    count: numbersOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+
+const a = prompt("one of the last watched films?", ""),
+      b = prompt("how much you rate it?", ""),
+      c = prompt("one of the last watched films?", ""),
+      d = prompt("how much you rate it?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
